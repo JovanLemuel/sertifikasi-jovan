e7c86e8fe03cb5a448857ede0ea9920e00f9d963
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2024-11-23 13:19:46 +0700

    set routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 });
+
+Route::resource('buku', BukuController::class);
+Route::resource('kategori', KategoriController::class);
+Route::resource('user', UserController::class);
